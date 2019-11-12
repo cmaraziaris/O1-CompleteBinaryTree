28c500d68814a9f69edb1da6292dd4edee03c6f9
spChalk <sdi1800209@di.uoa.gr>
2019-11-12 19:00:57 +0200

    Minor changes

--- Modules/BT_Impl/L_Implementation.c
+++ Modules/BT_Impl/L_Implementation.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <string.h>
 #include <ctype.h>
 #include <limits.h>
 #include <assert.h>
 #include "BTInit.h"
 
-int BTIsNil (BTNode p) { return (p == NULL); } /* If node p is NULL , return 1 , else return 0 */
+int BTIsNil (BTNode p) { return (p == NULL); } 	/* If node p is NULL , return 1 , else return 0 */
 
 BTTree BTCreate (void) {
-	BTTree bTree;  /* Create a main tree block */
+	BTTree bTree;  								/* Create a main tree block */
 	bTree = malloc (sizeof(struct main_tree));
 	bTree->tree = NULL;
 	bTree->size = 0;
-	//bTree->height = 0;
 	bTree->last = NULL;
 	printf("Tree created successfully!\n\n");
 
@@ -22,14 +20,14 @@ return bTree;
 
 int BTSize (BTTree p) { return p->size; }
 
-BTNode BTGetRoot(BTTree p) { return p->tree; } /* Return the node */
+inline BTNode BTGetRoot(BTTree p) { return p->tree; } /* Return the node */
 
-BTNode BTGetParent(BTTree head ,BTNode node) {
+inline BTNode BTGetParent(BTTree head ,BTNode node) {
 	// if ((!head->tree)||(!node->parent))  /* If node's parent is NULL return NULL */
 	// 	return NULL;
 	/*else*/  return node->parent;  /* Else return the node's parent */
 }
-BTNode BTGetChildLeft(BTTree head, BTNode node)
+inline BTNode BTGetChildLeft(BTTree head, BTNode node)
 {
 	if ((BTIsNil(BTGetRoot(head)))||(BTIsNil(node))) /* If the tree or the given node is NULL, return NULL */
 		return NULL;
@@ -40,7 +38,7 @@ BTNode BTGetChildLeft(BTTree head, BTNode node)
 		//else  return NULL;
 	}
 }
-BTNode BTGetChildRight(BTTree head, BTNode node) /* If the tree or the given node is NULL, return NULL */
+inline BTNode BTGetChildRight(BTTree head, BTNode node) /* If the tree or the given node is NULL, return NULL */
 {
 	if ((BTIsNil(BTGetRoot(head)))||(BTIsNil(node)))
 		return NULL;
@@ -51,10 +49,9 @@ BTNode BTGetChildRight(BTTree head, BTNode node) /* If the tree or the given nod
 		//else  return NULL;		
 	}
 }
-BTItem BTGetItem(BTNode head,BTNode node)
+inline BTItem BTGetItem(BTNode head,BTNode node)
 {
-	if (node) /* If the item exists, return it */
-			return node->item;
+	if (node) return node->item;					/* If the item exists, return it */
 	else  return -1;
 }
 void BTInsertRoot(BTTree head, BTItem item)
@@ -137,98 +134,98 @@ void BTRemoveLeaf(BTTree head,BTNode node)
 	head->size--;
 	return;
 }
-void BTChange(BTNode node, BTItem item)
+inline void BTChange(BTNode node, BTItem item)
 {
 	if (node) {
 	node->item=item; /* Change the current node's item, with the given one */
 	return;
 	}
 }
-static void PrintFormat (BTNode head)
-{   		/* If the item is a number, use %d */	
-	if (BTGetItem(head,head)>=0 && BTGetItem(head,head)<ULONG_MAX)
-		printf("<%d> ",BTGetItem(head,head));	/* If the item is a character, print it the right way */
-	else if (isalpha(BTGetItem(head,head))&&isalpha(BTGetItem(head,head)))
-		printf("<%c> ",BTGetItem(head,head));		
-	else   /* If the item is a string, print it  */
-		printf("<%s> ",BTGetItem(head,head));
-	return;
-}
-static void BTVisit (int key,BTTree head,BTNode node,void (*visit) (BTNode root))
-{
-	visit=PrintFormat;
-	if (!key)     /* Based on the key, execute the right recursive traversal */
-	{
-		if (node==NULL)
-			return;
+// static void PrintFormat (BTNode head)
+// {   		/* If the item is a number, use %d */	
+// 	if (BTGetItem(head,head)>=0 && BTGetItem(head,head)<ULONG_MAX)
+// 		printf("<%d> ",BTGetItem(head,head));	/* If the item is a character, print it the right way */
+// 	else if (isalpha(BTGetItem(head,head))&&isalpha(BTGetItem(head,head)))
+// 		printf("<%c> ",BTGetItem(head,head));		
+// 	else   /* If the item is a string, print it  */
+// 		printf("<%s> ",BTGetItem(head,head));
+// 	return;
+// }
+// static void BTVisit (int key,BTTree head,BTNode node,void (*visit) (BTNode root))
+// {
+// 	visit=PrintFormat;
+// 	if (!key)     /* Based on the key, execute the right recursive traversal */
+// 	{
+// 		if (node==NULL)
+// 			return;
 
-		visit(node);  /* Pre-Order Traversal */
-		BTVisit (0,head,BTGetChildLeft(head,node),visit);	
-		BTVisit (0,head,BTGetChildRight(head,node),visit);
-	}
-	else if (key==1)
-	{ 
-		if (node==NULL)
-			return;
-						/* In-Order Traversal */
-		BTVisit (1,head,BTGetChildLeft(head,node),visit);	
-		visit(node);
-		BTVisit (1,head,BTGetChildRight(head,node),visit);
-	}
-	else 
-	{ 
-		if (node==NULL)
-			return;
-						/* Post-Order Traversal */
-		BTVisit (2,head,BTGetChildLeft(head,node),visit);	
-		BTVisit (2,head,BTGetChildRight(head,node),visit);
-		visit(node);
-	}	
-}
-void BTPreOrder(BTTree head,void (*visit) (BTNode node))
-{
-	BTVisit(0,head,head->tree,visit);
-	return;
-}
-void BTInOrder(BTTree head,void (*visit) (BTNode node))
-{ 
-	BTVisit(1,head,head->tree,visit);
-	return;
-}
-void BTPostOrder(BTTree head,void (*visit) (BTNode node))
-{
-	BTVisit(2,head,head->tree,visit);
-	return;
-}
-//int BTHeight(BTTree p) { return p->height; }
+// 		visit(node);  /* Pre-Order Traversal */
+// 		BTVisit (0,head,BTGetChildLeft(head,node),visit);	
+// 		BTVisit (0,head,BTGetChildRight(head,node),visit);
+// 	}
+// 	else if (key==1)
+// 	{ 
+// 		if (node==NULL)
+// 			return;
+// 						/* In-Order Traversal */
+// 		BTVisit (1,head,BTGetChildLeft(head,node),visit);	
+// 		visit(node);
+// 		BTVisit (1,head,BTGetChildRight(head,node),visit);
+// 	}
+// 	else 
+// 	{ 
+// 		if (node==NULL)
+// 			return;
+// 						/* Post-Order Traversal */
+// 		BTVisit (2,head,BTGetChildLeft(head,node),visit);	
+// 		BTVisit (2,head,BTGetChildRight(head,node),visit);
+// 		visit(node);
+// 	}	
+// }
+// void BTPreOrder(BTTree head,void (*visit) (BTNode node))
+// {
+// 	BTVisit(0,head,head->tree,visit);
+// 	return;
+// }
+// void BTInOrder(BTTree head,void (*visit) (BTNode node))
+// { 
+// 	BTVisit(1,head,head->tree,visit);
+// 	return;
+// }
+// void BTPostOrder(BTTree head,void (*visit) (BTNode node))
+// {
+// 	BTVisit(2,head,head->tree,visit);
+// 	return;
+// }
+// //int BTHeight(BTTree p) { return p->height; }
 
-void BTLevelOrder(BTTree head,void (*visit) (BTNode node))
-{
-	visit=PrintFormat;
-	int size=BTSize(head),i=0,j=0;
-	BTNode arr[size],root=head->tree;
-	arr[i++]=root;
-	/* Create an array of Nodes and store every node in it*/
-	while(j<size)
-	{
-		visit(root);
-		if (root->left !=NULL) arr[i++]=root->left;
-		if (root->right!=NULL) arr[i++]=root->right;
-		root=arr[++j];  /* When the children of a node are stored in the array with the index "i", */
-	}							/* the root becomes the node pointed by index "j" */
-	printf("\n");
-	return;
-}
-static void DestroyRec (BTNode head)
-{
-	if (!head) return;
-	DestroyRec (head->left);  /* Traverse the tree */
-	DestroyRec (head->right);
-	head->left=NULL; head->right=NULL;
-	free(head);  /* Free its nodes */
-}
-void BTDestroy (BTTree head) 
-{
-	if (!head->tree) return;
-	DestroyRec(head->tree); return;
-}
+// void BTLevelOrder(BTTree head,void (*visit) (BTNode node))
+// {
+// 	visit=PrintFormat;
+// 	int size=BTSize(head),i=0,j=0;
+// 	BTNode arr[size],root=head->tree;
+// 	arr[i++]=root;
+// 	/* Create an array of Nodes and store every node in it*/
+// 	while(j<size)
+// 	{
+// 		visit(root);
+// 		if (root->left !=NULL) arr[i++]=root->left;
+// 		if (root->right!=NULL) arr[i++]=root->right;
+// 		root=arr[++j];  /* When the children of a node are stored in the array with the index "i", */
+// 	}							/* the root becomes the node pointed by index "j" */
+// 	printf("\n");
+// 	return;
+// }
+// static void DestroyRec (BTNode head)
+// {
+// 	if (!head) return;
+// 	DestroyRec (head->left);  /* Traverse the tree */
+// 	DestroyRec (head->right);
+// 	head->left=NULL; head->right=NULL;
+// 	free(head);  /* Free its nodes */
+// }
+// void BTDestroy (BTTree head) 
+// {
+// 	if (!head->tree) return;
+// 	DestroyRec(head->tree); return;
+// }
--- Modules/CBT_Impl/CBTImplementation.c
+++ Modules/CBT_Impl/CBTImplementation.c
@@ -7,28 +7,28 @@
 
 static void InsertFarLeft (CBTTree tree,CBTNode node ,CBTItem item) {
     
-    if (!node->left){ 	// We've found the far left node
+    if (BTIsNil(BTGetChildLeft(tree,node))){ 	                        // We've found the far left node
 	    BTInsertLeft (tree,node,item); 
         return; 
     }
-    InsertFarLeft(tree,node->left,item); // Continue to the left
+    InsertFarLeft(tree,BTGetChildLeft(tree,node),item);        // Continue to the left
 }
 
 static void RecInsert (CBTTree tree,CBTNode parent,CBTNode last,CBTItem item) {
     
-    if (!parent) InsertFarLeft(tree,tree->tree,item);  // If the tree is full, insert the node far left
-    else if (parent->right==last)		       // If the last node is a right child
-        RecInsert(tree,parent->parent,parent,item);    // Go up to its parent
-    else {					       // If the last node is a left child
-        (!parent->right)? BTInsertRight(tree,parent,item):  // If its right brother in NULL, insert right
-        InsertFarLeft(tree,parent->right,item);	       // Else, insert the node as left as possible
+    if (BTIsNil(parent)) InsertFarLeft(tree,BTGetRoot(tree),item);           // If the tree is full, insert the node far left
+    else if (BTGetChildRight(tree,parent) == last)		                        // If the last node is a right child
+        RecInsert(tree,BTGetParent(tree,parent),parent,item);             // Go up to its parent
+    else {					                                    // If the last node is a left child
+        (BTIsNil(BTGetChildRight(tree,parent)))? BTInsertRight(tree,parent,item) :      // If its right brother in NULL, insert right
+        InsertFarLeft(tree,BTGetChildRight(tree,parent),item);	                // Else, insert the node as left as possible
     } 
 }
 
 void CBTInsertLast(CBTTree tree,BTItem item) {
 
-    if (!tree->tree) BTInsertRoot(tree,item);		// If we have 0 nodes , we insert the item instantly
-    else RecInsert(tree,CBTGetLast(tree)->parent,tree->last,item);   
+    if (BTIsNil(BTGetRoot(tree))) BTInsertRoot(tree,item);		            // If we have 0 nodes , we insert the item instantly
+    else RecInsert( tree , BTGetParent(tree,CBTGetLast(tree)) , CBTGetLast(tree),item );   
 }  
 
 CBTNode CBTGetLast(CBTTree head) { return head->last; }
