32ade4e7ebb8ca09d35fb7a96f92314c69f8ca11
spChalk <sdi1800209@di.uoa.gr>
2020-03-06 17:37:04 +0200

    Added option to insert integers or strings

--- CBT_Main/Main.c
+++ CBT_Main/Main.c
@@ -3,39 +3,97 @@
 #include <time.h>
 #include "CBTInit.h"
 
+#ifdef INT
+
+void printItem(void * item) {
+	printf("(%d)\n" , *(int *)item);
+}
+
+void destroyItem(void * item) {
+	if (item != NULL) free((int *)item);
+}
+
+int* createItem(int value) {
+	int* pointer = malloc(sizeof(int));		
+	*pointer = value;						
+	return pointer;
+}
+
+#endif
+
+#ifdef STR
+
+#include <string.h>
+
+static char * randomStrGen (void) {
+	uint8_t size = rand()%10 + 1;
+	char * str = malloc (size * sizeof(char));
+	for (uint8_t i = 0; i < size - 1; i++) {
+		str[i] = (rand()%(122 - 65)) + 65;
+	}
+	str[size - 1] = '\0';
+	return str;
+}
+
+void printItem(void * item) {
+	printf("(%s)\n" , (char *)item);
+}
+
+void destroyItem(void * item) {
+	if (item != NULL) free((char *)item);
+}
+
+char* createItem(char * value) {
+	char* pointer = strdup(value);					
+	return pointer;
+}
+
+#endif
+
+
 int main(int argc , char* argv[])
 {
-	if(argc != 4) {			
+	if (argc != 4) {			
 		printf("Usage : ./O1_cbt  <size_of_tree>  <minimum_num_of_elements>  <maximum_num_of_elements> \n");
 		exit(EXIT_FAILURE);
 	}
-	const unsigned int size = atoi(argv[1]);
+
+	const uint32_t size = atoi(argv[1]);
 	#define MINNUM atoi(argv[2])
 	#define MAXNUM atoi(argv[3])
 
 	clock_t t; 
     
-	const CBTTree CBT = BTCreate();							/*	Create a tree  */
+	const CBTree CBT = BTCreate(destroyItem , printItem);	/*	Create a tree  */
 
-	for (unsigned int i = 0; i < size ; i++)
-		CBTInsertLast(CBT , rand()%50);
+	for (uint32_t i = 0; i < size ; i++)
+	#ifdef INT
+		CBTInsertLast(CBT , createItem(rand()%50));
+	#else
+		CBTInsertLast(CBT , createItem(randomStrGen()));
+	#endif
 
-	printf("Inserted %u elements in the tree.\n",size);
+	printf("Inserted %u elements in the tree.\n" , size);
 	printf("Printing tree . .\n\n");
-	BTreePrint(CBT,BTGetRoot(CBT),2);
 
-	for (unsigned int i = 0 ; i < size ; i++)	
+	BTreePrint(CBT , BTGetRoot(CBT) , 2);
+
+	for (uint32_t i = 0 ; i < size ; i++)	
 		CBTRemove(CBT , CBTGetLast(CBT));
 	printf("CBTree Destroyed.\n\n");
 
 	printf("Let's add some more elemets . . .\n\n\n");
 
- for (unsigned int j=MINNUM ; j<=MAXNUM ; j*=10) {
+ for (uint32_t j=MINNUM ; j<=MAXNUM ; j*=10) {
 	 
     t = clock();
 
-	for (unsigned int i = 0 ; i < j ; i++) 					/*	Insert elements	 */
-		CBTInsertLast(CBT , rand()%1000000);
+	for (uint32_t i = 0 ; i < j ; i++) 					/*	Insert elements	 */
+	#ifdef INT
+		CBTInsertLast(CBT , createItem(rand()%1000000));
+	#else
+		CBTInsertLast(CBT , createItem(randomStrGen()));
+	#endif
 
 	t = clock() - t;
 
@@ -43,7 +101,7 @@ int main(int argc , char* argv[])
 
 	t = clock();
  	
-	 for (unsigned int i = 0 ; i < j ; i++)					/*	Delete them  */
+	 for (uint32_t i = 0 ; i < j ; i++)					/*	Delete them  */
 		CBTRemove(CBT , CBTGetLast(CBT));
 	
 	t = clock() - t;
